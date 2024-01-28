7aa154adc4be018f6ff312a6f59dbbd1f35f5d5a
Shivam Mishra <113275391+Shivammishra03@users.noreply.github.com>
2024-01-29 00:52:23 +0530

    changes added

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="p4 text-lg border-b border-gray:50 flex gap-2 item-center">
-        <div class="i-logos-nuxt-icon"></div>
+        <div class="i-logos-nuxt-icon"><img src="/favicon.ico"/></div>
         <span text-xl>learn.nuxt.com</span> 
     </nav>
 </template>
